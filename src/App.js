3323a33aeadedc92164eeb84ba5cffecd05d3f3c
RAJAN SHARMA <rajan.kumar@geeky.dev>
2023-03-04 18:05:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AddUrlComponent from './components/AddUrlComponent';
+import ViewUrlComponent from './components/ViewUrlComponent';
+
+function App () {
+  return (
+    <div className='App container mt-5'>
+      <AddUrlComponent/>
+      <ViewUrlComponent/>
+    </div>
+  )
+}
+
+export default App;
